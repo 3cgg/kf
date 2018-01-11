497d3ebd47b3160052fcd1dc098bb243e28e9a8e
J <3cgg@163.com>
2018-01-11 11:05:31 +0800

    -

--- src/main/java/me/libme/module/kafka/fn/logger/ReadLogger2File.java
+++ src/main/java/me/libme/module/kafka/fn/logger/ReadLogger2File.java
@@ -18,9 +18,9 @@ import java.util.Optional;
  */
 public class ReadLogger2File {
 
-    public static final String TOPIC_PARTITION="--kf.topicPartition";
+    public static final String TOPIC_PARTITION="--kf.readLogger2file.topicPartition";
 
-    public static final String FILE="--kf.file";
+    public static final String FILE="--kf.readLogger2file.file";
 
     private String filePath;
 
